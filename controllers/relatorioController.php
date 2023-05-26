15f1411d030010e8121104b1b0f1886297c24885
Matheus Suzart <mathsuzart@outlook.com>
2023-05-25 23:36:59 -0300

    check-login-relatorio

--- controllers/relatorioController.php
+++ controllers/relatorioController.php
@@ -1,4 +1,21 @@
 <?php
 class relatorioController extends Controller {
-    
+    public function__construct(){
+        parent::__construct();
+
+        $this->user = new User();
+        if(!$this->user->checkLogin()){
+            header("Location:".BASE_URL."login");
+            exit;
+        }
+
+    }
+
+    public function index(){
+        $data = array();
+
+
+
+        $this->loadTemplate('relatorio',$data)
+    }
 }
